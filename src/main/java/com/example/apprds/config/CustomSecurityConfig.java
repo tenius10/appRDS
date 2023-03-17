43cb781916a3601e0eabdfeea9ba97895483ff66
johyuna <teniuscom@gmail.com>
2023-03-18 02:28:09 +0900

    push commit

--- src/main/java/com/example/apprds/config/CustomSecurityConfig.java
+++ src/main/java/com/example/apprds/config/CustomSecurityConfig.java
@@ -1,2 +1,57 @@
-package com.example.apprds.config;public class CustomSecurityConfig {
+package com.example.apprds.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@Log4j2
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+@RequiredArgsConstructor
+public class CustomSecurityConfig{
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration=new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST","PUT","DELETE"));
+
+        configuration.setAllowedHeaders(Arrays.asList("Authorization","Cache-Control","Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception{
+        //AuthenticationManager 설정
+        AuthenticationManagerBuilder authenticationManagerBuilder
+                =http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager
+                =authenticationManagerBuilder.build();
+
+        http.authenticationManager(authenticationManager);
+
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //CORS 처리
+        http.cors(httpSecurityCorsConfigurer->{
+            httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
+        });
+        return http.build();
+    }
 }
